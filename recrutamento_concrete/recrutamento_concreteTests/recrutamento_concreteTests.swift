630f00a43892cb4a37d07652e54c56a399ea9dd7
arilson.carmo <arilson.carmo@gdcommunity.co.uk>
2016-04-18 20:34:28 -0300

    Add initial commit

--- recrutamento_concrete/recrutamento_concreteTests/recrutamento_concreteTests.swift
+++ recrutamento_concrete/recrutamento_concreteTests/recrutamento_concreteTests.swift
@@ -49,7 +49,7 @@ class recrutamento_concreteTests: XCTestCase {
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
+        
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
